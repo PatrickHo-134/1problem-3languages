c7e0103e8cfe6e084652059c8a2b9ed7e640b494
PatrickHo-134 <phat.ho.93@gmail.com>
2021-02-13 12:20:06 +1100

    wip day 4 js

--- javascript/day4.js
+++ javascript/day4.js
@@ -23,10 +23,35 @@ function convertArrayToDictionary(arr){
     return dictionary;
 }
 
-var passwords = inputDay4.map(function(s){if (s == "\n"){ return s } else { return s.replace("\n", "") }})
+function checkValidPassport(passport, optionalFields){
+    let passportFields = Object.keys(passport).sort();
+    let missingFields = requiredFields.filter(x => !passportFields.includes(x))
+                                      .sort();
+
+    return (JSON.stringify(missingFields) == JSON.stringify(optionalFields.sort()) ||
+            JSON.stringify(passportFields) == JSON.stringify(requiredFields.sort()));
+}
+
+var passports = inputDay4.map(function(s){if (s == "\n"){ return s } else { return s.replace("\n", "") }})
                          .reduce(function(a,b){ return a + " " + b })
                          .split("\n")
                          .map(function(s){ return s.trim().split(" ") })
                          .map(convertArrayToDictionary);
 
-console.log(Object.keys(passwords[0]));
+// PART 1
+let validPassportCount = passports.filter(function(p){return checkValidPassport(p, ['cid'])})
+                                  .length;
+console.log(validPassportCount);
+// => 239
+
+// PART 2
+function checkYearFields(value, digitNum=4, yearRange=[1920,2002]){
+    let year = parseInt(value);
+    let [minYear, maxYear] = yearRange;
+
+    if (value.length == digitNum && Number.isInteger(year)){
+        return (minYear <= year && year <= maxYear);
+    } else { return false }
+}
+
+console.log(checkBYR('2003'));
